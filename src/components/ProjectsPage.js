6d085d334acd6f7258793eb266e4b469634c76c6
Vasek Sekret <vasik.sekret@gmail.com>
2021-09-23 15:58:01 -0700

    Projects Page updates

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-
-const ProjectsPage = () => (
-	<h1>Test</h1>
-);
+class ProjectsPage extends React.Component {
+  render() {
+    return (
+    	<div className="wrapper">
+	      <div className="container">
+	      	<div className="item">1</div>
+	      	<div className="item">2</div>
+	      	<div className="item">3</div>
+	      	<div className="item">4</div>
+	      </div>
+	     </div>
+		);
+	}
+};
 
 export default ProjectsPage;
