2bf8c0315b1799d7adc242a233a43919c9c5b1b9
Vasek Sekret <vasik.sekret@gmail.com>
2021-09-23 17:44:18 -0700

    Adding grid and testing images

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 class ProjectsPage extends React.Component {
   render() {
+  	const image_url = '/images/favicon.ico';
     return (
-    	<div className="wrapper">
+    	<div className="content">
 	      <div className="container">
-	      	<div className="item">1</div>
+	      	<div className="item" style={{ backgroundImage : `url(${image_url})` }}><a rel="noopener noreferrer" href="https://www.canadanowfestival.com/?redirect=off" target="blank">Testing</a></div>
 	      	<div className="item">2</div>
 	      	<div className="item">3</div>
 	      	<div className="item">4</div>
