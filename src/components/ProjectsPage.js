35fecc3beb98c6981b87c8be3f131a2472a62672
Vasek Sekret <vasik.sekret@gmail.com>
2021-09-24 21:36:08 -0700

    Project Page Styling and GA Tracking

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,16 +1,56 @@
 import React from 'react';
 
+
 class ProjectsPage extends React.Component {
   render() {
-  	const image_url = '/images/p_canada.jpg';
-  	const width = '250px';
     return (
     	<div className="content">
-	      <div className="container">
-	      	<div className="item" style={{ backgroundImage : `url(${image_url})`, backgroundRepeat: 'no-repeat', height: '117px', backgroundPosition: 'center', backgroundSize: 'contain'}}></div>
-	      	<div className="item" style={{ backgroundImage : `url(${image_url})`, backgroundRepeat: 'no-repeat', height: '117px', backgroundPosition: 'center', backgroundSize: 'contain'}}></div>
-	      	<div className="item" style={{ backgroundImage : `url(${image_url})`, backgroundRepeat: 'no-repeat', height: '117px', backgroundPosition: 'center', backgroundSize: 'contain'}}></div>
-	      </div>
+	      	<div className="item">
+	      		<a rel="noopener noreferrer" href="https://www.canadanowfestival.com/?redirect=off" target="_blank" title="Open Canada Now site in a new tab">
+	      			<img src="images/p_canada.jpg" alt="Canada Now Title Treatment"/>
+	      			<div className="container">
+	      				<h4>Mitchells vs The Machines - Selfie Frame</h4>
+	      			</div>
+	      		</a>
+	      	</div>
+	      	<div className="item">
+	      		<a rel="noopener noreferrer" href="https://www.canadanowfestival.com/?redirect=off" target="_blank" title="Open Canada Now site in a new tab">
+	      			<img src="images/p_canada.jpg" alt="Canada Now Title Treatment"/>
+	      		</a>
+	      	</div>
+
+	      	<div className="item">
+	      		<a rel="noopener noreferrer" href="https://www.canadanowfestival.com/?redirect=off" target="_blank" title="Open Canada Now site in a new tab">
+	      			<img src="images/p_canada.jpg" alt="Canada Now Title Treatment"/>
+	      		</a>
+	      	</div>
+
+	      	<div className="item">
+	      		<a rel="noopener noreferrer" href="https://www.canadanowfestival.com/?redirect=off" target="_blank" title="Open Canada Now site in a new tab">
+	      			<img src="images/p_canada.jpg" alt="Canada Now Title Treatment"/>
+	      		</a>
+	      	</div>
+	      	<div className="item">
+	      		<a rel="noopener noreferrer" href="https://www.canadanowfestival.com/?redirect=off" target="_blank" title="Open Canada Now site in a new tab">
+	      			<img src="images/p_canada.jpg" alt="Canada Now Title Treatment"/>
+	      		</a>
+	      	</div>
+	      	<div className="item">
+	      		<a rel="noopener noreferrer" href="https://www.canadanowfestival.com/?redirect=off" target="_blank" title="Open Canada Now site in a new tab">
+	      			<img src="images/p_canada.jpg" alt="Canada Now Title Treatment"/>
+	      		</a>
+	      	</div>
+	      	<div className="item">
+	      		<a rel="noopener noreferrer" href="https://www.canadanowfestival.com/?redirect=off" target="_blank" title="Open Canada Now site in a new tab">
+	      			<img src="images/p_canada.jpg" alt="Canada Now Title Treatment"/>
+	      		</a>
+	      	</div>
+	      	
+	      	<div className="item">
+	      		<a rel="noopener noreferrer" href="https://www.canadanowfestival.com/?redirect=off" target="_blank" title="Open Canada Now site in a new tab">
+	      			<img src="images/p_canada.jpg" alt="Canada Now Title Treatment"/>
+	      		</a>
+	      	</div>
 	     </div>
 		);
 	}
