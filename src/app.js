e029518808ea5972d4b0b54fbdf9a0c0aebdd995
Vasek Sekret <vasik.sekret@gmail.com>
2021-09-26 15:55:49 -0700

    Removed reducer

--- src/app.js
+++ src/app.js
@@ -6,13 +6,9 @@ import configureStore from './store/configureStore';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const store = configureStore();
-
 
 const jsx = (
-  <Provider store={store}>
     <AppRouter />
-  </Provider>
 );
 
 ReactDOM.render(jsx, document.getElementById('app'));
