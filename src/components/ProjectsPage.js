a8382624953cc5387d623b80137feb27f1c45889
Vasek Sekret <vasik.sekret@gmail.com>
2021-09-25 17:48:35 -0700

    Divider styling

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -9,7 +9,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.playlistnetflix.com/home/" target="_blank" title="Open Playlist site in a new tab">
 	      			<img src="images/playlist_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Playlist | Netflix</h4>
+	      				<h4>Playlist <i>|</i> Netflix</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -18,7 +18,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.wannatalkaboutit.com/" target="_blank" title="Open Wanna Talk About It site in a new tab">
 	      			<img src="images/resourcesSite_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Wanna Talk About It | Netflix</h4>
+	      				<h4>Wanna Talk About It <i>|</i> Netflix</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -27,7 +27,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.netflixqueue.com/" target="_blank" title="Open Netflix Queue site in a new tab">
 	      			<img src="images/queue_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Netflix Queue | Netflix</h4>
+	      				<h4>Netflix Queue <i>|</i> Netflix</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -36,7 +36,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.canadanow.us/" target="_blank" title="Open Canada Now site in a new tab">
 	      			<img src="images/canadaNow_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Canada Now | Telefilm Canada</h4>
+	      				<h4>Canada Now <i>|</i> Telefilm Canada</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -45,7 +45,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.candymanmovie.com/impact/" target="_blank" title="Open Candyman site in a new tab">
 	      			<img src="images/candyman_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Candyman | Universal Pictures</h4>
+	      				<h4>Candyman <i>|</i> Universal Pictures</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -54,7 +54,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.halloweenmovie.com/" target="_blank" title="Open Halloween Kills site in a new tab">
 	      			<img src="images/halloween_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Halloween Kills | Universal Pictures</h4>
+	      				<h4>Halloween Kills <i>|</i> Universal Pictures</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -63,7 +63,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.respectmovie.ca/home/" target="_blank" title="Open Respect site in a new tab">
 	      			<img src="images/respect_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Respect | Universal Pictures</h4>
+	      				<h4>Respect <i>|</i> Universal Pictures</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -72,7 +72,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.codyjohnsonfilm.com/" target="_blank" title="Open Dear Rodeo site in a new tab">
 	      			<img src="images/dearRodeo_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Dear Rodeo | Trafalgar Releasing</h4>
+	      				<h4>Dear Rodeo <i>|</i> Trafalgar Releasing</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -81,7 +81,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.pawpatrol.movie/" target="_blank" title="Open Paw Patrol site in a new tab">
 	      			<img src="images/paw_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Paw Patrol | Paramount Pictures</h4>
+	      				<h4>Paw Patrol <i>|</i> Paramount Pictures</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -90,7 +90,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.venom.movie/" target="_blank" title="Open Venom site in a new tab">
 	      			<img src="images/venom_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Venom | Sony Pictures</h4>
+	      				<h4>Venom <i>|</i> Sony Pictures</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -99,7 +99,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.disneyscruellatickets.ca/" target="_blank" title="Open Cruella site in a new tab">
 	      			<img src="images/cruella_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Cruella | Disney</h4>
+	      				<h4>Cruella <i>|</i> Disney</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -108,7 +108,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.gundamovie.com/" target="_blank" title="Open Gunda site in a new tab">
 	      			<img src="images/gunda_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Gunda | Neon Rated</h4>
+	      				<h4>Gunda <i>|</i> Neon Rated</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -117,7 +117,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://listen.iamhalsey.com/" target="_blank" title="Open Halsey Music site in a new tab">
 	      			<img src="images/halsey_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Halsey | Halsey</h4>
+	      				<h4>Halsey <i>|</i> Halsey</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -126,7 +126,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://tickets.oscar-shorts.com/" target="_blank" title="Open Oscar Shorts site in a new tab">
 	      			<img src="images/shorts_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Oscar Shorts | Shorts TV</h4>
+	      				<h4>Oscar Shorts <i>|</i> Shorts TV</h4>
 	      			</div>
 	      		</a>
 	      	</div>
@@ -135,7 +135,7 @@ class ProjectsPage extends React.Component {
 	      		<a rel="noopener noreferrer" href="https://www.languagelessonsfilm.com/" target="_blank" title="Open Language Lessons site in a new tab">
 	      			<img src="images/languageLessons_image.jpg" alt="Playlist | Netflix Image"/>
 	      			<div className="container">
-	      				<h4>Language Lessons | Shout Factory</h4>
+	      				<h4>Language Lessons <i>|</i> Shout Factory</h4>
 	      			</div>
 	      		</a>
 	      	</div>
